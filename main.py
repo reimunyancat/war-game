03a7578e927a0ee9f5d8d0934d567fb7e167b72b
reimunyancat <95156029+reimunyancat@users.noreply.github.com>
2024-05-08 15:18:09 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
-import datetime, time, pygame, sys, random
+import datetime, time, sys, random
 # import PIL.Image
 from random import randint
+# import pygame
 # from pygame.locals import *
 
 # FPS = 30
 # FramePerSec = pygame.time.Clock()
 
 
-SCREEN_WIDTH = 1000
+# SCREEN_WIDTH = 1000
 
-SCREEN_HEIGHT = 1000
+# SCREEN_HEIGHT = 1000
 
 money = 1000
 soldier = 0
 weapon = 0
 weapon_gan = 1
-weapon_name = {0 : '모래', 1 : '돌', 2 : '나무검', 3 : '돌검'}
+weapon_gan_take = 10
+weapon_name = {0 : '모래', 1 : '돌', 2 : '나무검', 3 : '돌검', 4 : '낫', 5 : '망치', 6 : '도끼', 7 : '칼', 8 : '노트북', 9 : '톱', 10 : '수류탄'}
 money_take = 100
 weapon_ganha_take = 100
 weapon_random_front = '100%'
@@ -24,10 +26,12 @@ weapon_random_back = 100
 enemycountry = 0
 enemycountry_name = {0 : '일본', 1 : '중국', 2 : '몽골', 3 : '태국'}
 enemycountry_defence = {0 : 10, 1 : 30, 2 : 50, 3 : 100}
-enemycountry_attack = {0 : 2, 1 : 5, 2 : 10, 3 : 20}
+enemycountry_attack = {0 : 5, 1 : 10, 2 : 20, 3 : 25}
+enemycountry_soldier_reward = {0 : 2, 1 : 5, 2 : 10, 3 : 30}
+enemycountry_money_reward = {0 : 1000, 1 : 5000, 2 : 10000, 3 : 30000}
 
 def printline():
-    print("\n==============================\n")
+    print("\n\n==============================\n")
 
 def tutorial():
     printline()
@@ -35,7 +39,6 @@ def tutorial():
     print("전투력은 부대수 = HP, 무기 = 공격력입니다")
     print("메인화면에서 잔액과 부대수와 무기강화현황을 볼 수 있습니다")
     print("나라를 점령할수록 돈벌기 수익이 증가합니다\n")
-
 
 def make_money():
     printline()
@@ -69,6 +72,7 @@ def make_money2():
     print("홀짝 게임을 시작합니다!\n성공하신다면 베팅금액을 4배로 획득하실수 있습니다.\n실패하시면 베팅금액을 잃습니다.")
     
     while(True):
+        print(f"현재 돈 : {money}")
         serve_input = input("하시려면 1, 나가시려면 2를 입력하세요\n")
         if serve_input == '2':
             break
@@ -81,7 +85,7 @@ def make_money2():
         user_choice = input("홀수인지 짝수인지 선택하세요 (홀수/짝수): ")
         
         if user_choice not in ['홀수', '짝수']:
-            print("제대로 입력도 못하노")
+            print("제대로 입력")
             return
 
         computer_number = randint(1, 100)
@@ -96,7 +100,7 @@ def make_money2():
 
 def make_money3():
     global money
-    printline()
+    
     def initialize_deck():
         suits = ['스페이드', '다이아몬드', '하트', '클로버']
         ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
@@ -114,13 +118,27 @@ def make_money3():
                 num_aces += 1
             else:
                 value += int(card['rank'])
-        value += num_aces
         for i in range(num_aces):
-            if value + 11 <= 21:
+            if value + 11 <= 21 and num_aces - i == 1: 
                 value += 11
+            else:
+                value += 1
         return value
+    
+    def card_priority(card):
+        if card['rank'] == 'A':
+            return 1
+        elif card['rank'] in ['K', 'Q', 'J']:
+            return 2
+        elif card['rank'] in ['10', '9', '8', '7', '6', '5', '4', '3', '2']:
+            return int(card['rank'])
+        else:
+            return 0
+
     print("블랙잭 게임을 시작합니다!\n성공하시면 배팅금액의 10배로 획득하실수 있습니다\n실패하시면 배팅금액을 잃습니다.")
+    
     while True:
+        print(f"현재 돈 : {money}")
         serve_input = input("하시려면 1, 나가시려면 2를 입력하세요\n")
         if serve_input == '2':
             break
@@ -135,15 +153,14 @@ def make_money3():
         player_hand = [deck.pop(), deck.pop()]
         dealer_hand = [deck.pop(), deck.pop()]
 
-        print("딜러의 카드:")
-        print(f" ** {dealer_hand[1]['rank']} of {dealer_hand[1]['suit']} **")
         print("플레이어의 카드:")
         for card in player_hand:
             print(f" {card['rank']} of {card['suit']}")
+            
         player_hand_value = calculate_hand_value(player_hand)
-        dealer_hand_value = calculate_hand_value(dealer_hand)
+        print(f"플레이어의 현재 카드 합계: {player_hand_value}")
 
-        while player_hand_value < 21:
+        while player_hand_value < 22:
             action = input("카드를 더 받으시겠습니까? (y/n): ").lower()
             if action == 'y':
                 player_hand.append(deck.pop())
@@ -155,11 +172,14 @@ def make_money3():
 
         if player_hand_value > 21:
             print("카드 합계가 21을 넘었습니다. 플레이어 패배!")
+            money -= betting
             return
 
-        while dealer_hand_value < 17:
-            dealer_hand.append(deck.pop())
+        while True:
             dealer_hand_value = calculate_hand_value(dealer_hand)
+            if dealer_hand_value >= 17:
+                break
+            dealer_hand.append(deck.pop())
 
         print("딜러의 최종 카드:")
         for card in dealer_hand:
@@ -168,12 +188,23 @@ def make_money3():
 
         if dealer_hand_value > 21 or player_hand_value > dealer_hand_value:
             print("플레이어 승리!")
-            money += betting*10-money
+            money += betting * 10 - money
         elif dealer_hand_value > player_hand_value:
             print("딜러 승리!")
             money -= betting
         else:
-            print("무승부!")
+            player_priority = max([card_priority(card) for card in player_hand])
+            dealer_priority = max([card_priority(card) for card in dealer_hand])
+
+            if player_priority > dealer_priority:
+                print("플레이어 승리!")
+                money += betting * 10 - money
+            elif player_priority < dealer_priority:
+                print("딜러 승리!")
+                money -= betting
+            else:
+                print("무승부")
+        
 
 
 def make_soldier():
@@ -190,67 +221,82 @@ def make_soldier():
 
 
 def weapon_ganha():
-    global money, weapon_ganha_take, weapon_gan, weapon, weapon_name, weapon_random, weapon_random_back, weapon_random_front
+    global money, weapon_ganha_take, weapon_gan, weapon, weapon_name, weapon_random_back, weapon_random_front, weapon_gan_take
+    
     printline()
-    print(f"현재 강화 수치는 {weapon_name[weapon]} {weapon}입니다\n강화 비용은 {weapon_ganha_take}입니다\n강화 확률은 {weapon_random_front}입니다.")
-    serve_input = input("강화하시려면 '/강화'를 입력하세요\n강화를 멈추려면'/튀튀'를 입력하세요\n")
-    if serve_input == '/강화':
-        while True:
+    while True:
+        print(f"현재 강화 수치는 {weapon_name[weapon]} ({weapon})입니다\n강화 비용은 {int(weapon_ganha_take)}입니다\n강화 확률은 {weapon_random_front}입니다.")
+        print(f"현재 돈 : {int(money)}")
+        serve_input = input("강화하시려면 '/강화'를 입력하세요\n강화를 멈추려면 '/튀튀'를 입력하세요\n")
+
+        if serve_input == '/강화':
             if money - weapon_ganha_take < 0:
                 print("돈부터 벌으세요")
-                break
+                return
+
+            weapon_random = randint(1, 100)
             if weapon_random <= weapon_random_back:
                 weapon += 1
                 if weapon_gan == 1:
                     weapon_gan += 9
-                weapon_gan += 10
-                money -= weapon_ganha_take
-                weapon_ganha_take *= 1.05
-                weapon_random_back -= weapon_random_back * 0.21
-                weapon_random_front = str(weapon_random_back)+"%"
-                print("강화 성공!")
-                print(f"현재 강화 수치는 {weapon_name[weapon]} {weapon}입니다\n강화 비용은 {int(weapon_ganha_take)}입니다\n강화 확률은 {weapon_random_front}입니다.")
-                serve_input = input("강화하시려면 '/강화'를 입력하세요\n강화를 멈추려면'/튀튀'를 입력하세요\n")
-                if serve_input == '/강화':
-                    continue
-                elif serve_input == '/튀튀':
-                    break
                 else:
-                    print("에러")
+                    weapon_gan += 10
+                money -= int(weapon_ganha_take)
+                if weapon < 10:
+                    weapon_gan_take *= 1.2
+                    weapon_ganha_take *= 1.1
+                elif weapon < 20:
+                    weapon_gan_take *= 1.5
+                    weapon_ganha_take *= 1.2
+                weapon_random_back -= weapon_random_back * 0.2
+                weapon_random_front = str(weapon_random_back) + "%"
+                print("\n강화 성공!")
+                printline()
             else:
                 money -= weapon_ganha_take
-                print("강화 실패 ㅋㅋㅋㅋ")
-                break
-    elif serve_input == '/튀튀':print("에휴")
-    else:
-        print("뭐라카노")
+                print("\n강화 실패 ㅋㅋㅋㅋ")
+                printline()
+            
+        elif serve_input == '/튀튀':
+            print("에휴")
+            break
+        else:
+            print("뭐라카노")
+
 
 def war():
+    global enemycountry_defence, soldier, enemycountry, money
     printline()
     while True:
-        global enemycountry_defence, soldier
         print(f"이번 나라는 {enemycountry_name[enemycountry]}입니다.")
         print("전쟁을 시작하시려면 1, 나가시려면 2를 입력하세요\n")
         serve_input = int(input(""))
         if serve_input == 1:
             while True:
                 printline()
+                if enemycountry_defence[enemycountry] < 0:
+                    enemycountry_defence[enemycountry] = 0
                 print(f"{country_name} 군인 수 : {soldier}명  공격력 : {weapon_gan} ==================== {enemycountry_name[enemycountry]} 군인 수 : {enemycountry_defence[enemycountry]}  공격력 : {enemycountry_attack[enemycountry]}")
                 if enemycountry_defence[enemycountry] <= 0:
                     printline()
                     print(f"{enemycountry_name[enemycountry]}를 이겼습니다!")
+                    soldier += enemycountry_soldier_reward[enemycountry]
+                    money += enemycountry_money_reward[enemycountry]
+                    print(f"군인 {enemycountry_soldier_reward[enemycountry]}명과  돈 {enemycountry_money_reward[enemycountry]}원을 얻었습니다")
                     break
                 if soldier <= 0:
                     printline()
-                    print(f"{enemycountry_name[enemycountry]}에게 졌습니다...")
                     if soldier < 0: soldier = 0 
+                    print(f"{enemycountry_name[enemycountry]}에게 졌습니다...")
                     break
                 enemycountry_defence[enemycountry] -= weapon_gan
                 soldier -= enemycountry_attack[enemycountry]
                 time.sleep(1)
                 printline()
+                enemycountry += 1
 
         elif serve_input == 2:
+            print("에휴")
             break
     
 printline()
@@ -267,7 +313,7 @@ print("\n'/도움말'을 입력하세요")
 while True:
     printline()
     print(f"국가 이름 : {country_name}  유저 이름 : {main_name}")
-    print(f"돈 : {money}  군인 수 : {soldier}  무기 : {weapon_name[weapon]} (공격력 : {weapon_gan})")
+    print(f"돈 : {int(money)}  군인 수 : {soldier}  무기 : {weapon_name[weapon]} (공격력 : {int(weapon_gan)})")
     print("1: 돈벌기\n2: 부대모집\n3: 무기강화\n4: 전쟁시작\n5: 게임을 끄기\n")
     main_input = input()
     if main_input == "/도움말":
