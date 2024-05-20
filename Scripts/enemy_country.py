51ea0e4d8fff15efe320d53cf9c3a7396a5a5023
REIMU <lasco647@gmail.com>
2024-05-20 22:03:26 +0900

    wa

---
+++ Scripts/enemy_country.py
@@ -0,0 +1,7 @@
+class EnemyCountry:
+    def __init__(self, name, defence, attack, soldier_reward, money_reward):
+        self.name = name
+        self.defence = defence
+        self.attack = attack
+        self.soldier_reward = soldier_reward
+        self.money_reward = money_reward
